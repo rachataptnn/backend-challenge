41459464d8891034f6c17dde9f6b24232f44f13d
RachataRocket <rachata@rocket.in.th>
2025-03-10 15:10:20 +0700

    refactor/problem2: split lines of code to functions to know its Responsibilities

--- solutions/second/main.go
+++ solutions/second/main.go
@@ -8,73 +8,96 @@ import (
 func main() {
 	// input := "LLRR=" // 210122
 	// input := "==RLL" // 000210
-	// input := "=LLRR" // 221012
-	input := "RRL=R" // 012001
+	input := "=LLRR" // 221012
+	// input := "RRL=R" // 012001
 
 	min := zaiKwa(input)
 	fmt.Println(min)
 }
 
-func zaiKwa(input string) string {
-	runeInput := []rune(input)
+func zaiKwa(encoded string) string {
+	runeEncoded := []rune(encoded)
 
-	// init L 2 1 0
-	numL := 0
-	for i := len(runeInput) - 1; i >= 0; i-- {
-		if runeInput[i] == 'L' {
-			runeInput[i] = rune('0' + numL)
-			numL++
+	lastNumL := convertLToNums(runeEncoded)
+	convertRToNums(runeEncoded)
+	remapLRNums(runeEncoded, encoded)
+	convertEqToNums(runeEncoded)
+	result := mapFirstCharToNum(runeEncoded, encoded, lastNumL)
+
+	return result
+}
+
+// init L 2 1 0
+func convertLToNums(runeEncoded []rune) int {
+	lastNumL := 0
+	for i := len(runeEncoded) - 1; i >= 0; i-- {
+		if runeEncoded[i] == 'L' {
+			runeEncoded[i] = rune('0' + lastNumL)
+			lastNumL++
 		} else {
-			numL = 0
+			lastNumL = 0
 		}
 	}
 
-	// init R 0 1 2
+	return lastNumL
+}
+
+// init R 0 1 2
+func convertRToNums(runeEncoded []rune) int {
 	numR := 0
-	for i, v := range input {
-		if v == 'R' {
-			runeInput[i] = rune('0' + numR)
+	for i := 0; i < len(runeEncoded); i++ {
+		if runeEncoded[i] == 'R' {
+			runeEncoded[i] = rune('0' + numR)
 			numR++
 		} else {
 			numR = 0
 		}
 	}
 
-	// check if 'R' is not more the left side
-	modifiedInput := string(runeInput)
-	for i := range modifiedInput {
-		if input[i] == 'R' && i != 0 {
-			prevVal, _ := strconv.Atoi(string(modifiedInput[i-1]))
-			runeInput[i] = rune('0' + prevVal + 1)
-			modifiedInput = string(runeInput)
+	return numR
+}
+
+func remapLRNums(runeEncoded []rune, encoded string) {
+	for i := 0; i < len(runeEncoded); i++ {
+		if encoded[i] == 'R' && i != 0 {
+			prevVal := runeEncoded[i-1]
+			if prevVal == '=' {
+				runeEncoded[i] = '1'
+			} else {
+				runeEncoded[i] = prevVal + 1
+			}
 		}
 
-		if input[i] == 'L' && i != 0 {
-			prevVal, _ := strconv.Atoi(string(modifiedInput[i-1]))
-			currVal, _ := strconv.Atoi(string(modifiedInput[i]))
-
-			if prevVal <= currVal {
-				runeInput[i-1] = rune('0' + currVal + 1)
-				modifiedInput = string(runeInput)
+		if encoded[i] == 'L' && i != 0 {
+			prevVal := runeEncoded[i-1]
+			currVal := runeEncoded[i]
+			if prevVal <= currVal || prevVal == '=' {
+				runeEncoded[i-1] = currVal + 1
 			}
 		}
 	}
+}
 
-	for i, v := range runeInput {
+func convertEqToNums(runeEncoded []rune) {
+	for i, v := range runeEncoded {
 		if v == '=' && i == 0 {
-			runeInput[i] = '0'
+			runeEncoded[i] = '0'
 		} else if v == '=' {
-			runeInput[i] = runeInput[i-1]
+			runeEncoded[i] = runeEncoded[i-1]
 		}
 	}
+}
 
-	modifiedInput = string(runeInput)
-	if input[0] == 'L' {
-		modifiedInput = strconv.Itoa(numL) + modifiedInput
-	} else if input[0] == 'R' {
+func mapFirstCharToNum(runeEncoded []rune, encoded string, lastNumL int) string {
+	modifiedInput := string(runeEncoded)
+
+	switch encoded[0] {
+	case 'L':
+		modifiedInput = strconv.Itoa(lastNumL) + modifiedInput
+	case 'R':
 		lastR := 0
 		foundR := false
-		for i, v := range input {
+		for i, v := range encoded {
 			if v == 'R' {
 				foundR = true
 			}
@@ -87,7 +110,7 @@ func zaiKwa(input string) string {
 
 		firstPart := "0"
 		foundR = false
-		for i, v := range input {
+		for i, v := range encoded {
 			if v == 'R' {
 				foundR = true
 				firstPart += strconv.Itoa(i + 1)
@@ -98,12 +121,9 @@ func zaiKwa(input string) string {
 		}
 
 		modifiedInput = firstPart + secondPart
-
-		// modifiedInput = "0" + cumputed
-
-	} else if input[0] == '=' {
+	case '=':
 		modifiedInput = string(modifiedInput[0]) + modifiedInput
-	} else {
+	default:
 		modifiedInput = "0" + modifiedInput
 	}
 
